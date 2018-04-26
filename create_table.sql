f8035593d5e5586200b963ca72c991e1937f09d1
Noah Loomans <noahloomans@gmail.com>
2018-04-26 23:02:33 +0200

    Add create_table sql script

---
+++ create_table.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `guilds` (
+  `id` varchar(18) NOT NULL,
+  `ownerId` varchar(18) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `memberCount` int(11) NOT NULL,
+  `created` timestamp(6) NOT NULL,
+  `region` varchar(45) NOT NULL,
+  `splash` varchar(45) DEFAULT NULL,
+  `iconUrl` varchar(88) DEFAULT NULL,
+  `timestamp` timestamp(6) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `presence` (
+  `pk` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` varchar(18) NOT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `game` varchar(45) DEFAULT NULL,
+  `timestamp` timestamp(1) NOT NULL,
+  PRIMARY KEY (`pk`)
+);
+
+CREATE TABLE `users` (
+  `id` varchar(18) NOT NULL,
+  `guildId` varchar(18) DEFAULT NULL,
+  `username` varchar(32) NOT NULL,
+  `discriminator` smallint(4) NOT NULL,
+  `avatar` varchar(100) DEFAULT NULL,
+  `displayname` varchar(32) DEFAULT NULL,
+  `joined` varchar(45) DEFAULT NULL,
+  `color` varchar(45) DEFAULT NULL,
+  `bot` bit(1) DEFAULT NULL,
+  `timestamp` timestamp(6) NOT NULL,
+  PRIMARY KEY (`id`),
+);
