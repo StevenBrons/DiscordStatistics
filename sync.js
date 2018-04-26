63689914c2b2a0d4a9b1f4d40c1ad9036036e4fc
Noah Loomans <noahloomans@gmail.com>
2018-04-26 23:02:43 +0200

    Add debug info

--- sync.js
+++ sync.js
@@ -5,4 +5,16 @@ const env = require('node-env-file');
 // Import the secret values (e.g. private discord key) from .env
 env(__dirname + '/.env')
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('presenceUpdate', (oldMember, newMember) => {
+  console.log('oldPresence:', oldMember.presence.status);
+  console.log('newPresence:', newMember.presence.status);
+  console.log('oldMember:', oldMember);
+  console.log('newMember:', newMember);
+  debugger;
+});
+
 client.login(process.env.KEY);
