82681afd3dc3c495301a2e498991254828d7495a
Noah Loomans <noahloomans@gmail.com>
2018-04-28 11:45:15 +0200

    Finalize(?) create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,35 +1,40 @@
+DROP TABLE IF EXISTS `guilds`;
 CREATE TABLE `guilds` (
-  `id` varchar(18) NOT NULL,
-  `ownerId` varchar(18) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `memberCount` int(11) NOT NULL,
-  `created` timestamp NOT NULL,
-  `region` varchar(45) NOT NULL,
-  `splash` varchar(45) DEFAULT NULL,
-  `iconUrl` varchar(88) DEFAULT NULL,
-  `timestamp` timestamp NOT NULL,
+  `id` VARCHAR(18) NOT NULL,
+  `ownerId` VARCHAR(18) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `memberCount` INT(11) NOT NULL,
+  `created` DATETIME NOT NULL,
+  `region` VARCHAR(45) NOT NULL,
+  `splash` VARCHAR(45) DEFAULT NULL,
+  `iconUrl` VARCHAR(88) DEFAULT NULL,
+  `timestamp` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
 );
 
+DROP TABLE IF EXISTS `presence`;
 CREATE TABLE `presence` (
-  `pk` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` varchar(18) NOT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `game` varchar(45) DEFAULT NULL,
-  `timestamp` timestamp NOT NULL,
+  `pk` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` VARCHAR(18) NOT NULL,
+  `status` VARCHAR(45) DEFAULT NULL,
+  `game` VARCHAR(45) DEFAULT NULL,
+  `timestamp` DATETIME NOT NULL,
   PRIMARY KEY (`pk`)
 );
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` varchar(18) NOT NULL,
-  `guildId` varchar(18) DEFAULT NULL,
-  `username` varchar(32) NOT NULL,
-  `discriminator` smallint(4) NOT NULL,
-  `avatar` varchar(100) DEFAULT NULL,
-  `displayname` varchar(32) DEFAULT NULL,
-  `joined` varchar(45) DEFAULT NULL,
-  `color` varchar(45) DEFAULT NULL,
-  `bot` bit(1) DEFAULT NULL,
-  `timestamp` timestamp NOT NULL,
-  PRIMARY KEY (`id`),
+  `pk` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` VARCHAR(18) NOT NULL,
+  `guildId` VARCHAR(18) DEFAULT NULL,
+  `discriminator` SMALLINT NOT NULL,
+  `avatar` VARCHAR(100) DEFAULT NULL,
+  `displayname` VARCHAR(32) DEFAULT NULL,
+  `joined` VARCHAR(45) DEFAULT NULL,
+  `color` VARCHAR(45) DEFAULT NULL,
+  `bot` BIT(1) NOT NULL,
+  `timestamp` DATETIME NOT NULL,
+  -- In reality the pk is id + guildId, but guildId has to be null. So we will
+  -- check this before insertion
+  PRIMARY KEY (`pk`)
 );
