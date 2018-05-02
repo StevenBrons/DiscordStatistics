a40ab81ec8bb7e19ea037b0b1bd8962988e53e23
smp15 <smp15.programming@gmail.com>
2018-05-02 19:50:52 +0200

    Add guildmember to database

--- sync.js
+++ sync.js
@@ -1,17 +1,35 @@
 require('./lib/env');
 
 const Discord = require('discord.js');
+const Database = require('./lib/Database');
+
 const client = new Discord.Client();
+const database = new  Database();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const databaseConf = {
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+};
 
-client.on('presenceUpdate', (oldMember, newMember) => {
-  console.log('oldPresence:', oldMember.presence.status);
-  console.log('newPresence:', newMember.presence.status);
-  console.log('oldMember:', oldMember);
-  console.log('newMember:', newMember);
-});
+async function main() {
+  await database.connect(databaseConf);
 
-client.login(process.env.DISCORD_KEY);
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  
+  client.on('presenceUpdate', (oldMember, newMember) => {
+    console.log('oldPresence:', oldMember.presence.status);
+    console.log('newPresence:', newMember.presence.status);
+    console.log('oldMember:', oldMember);
+    console.log('newMember:', newMember);
+  
+    database.setGuildMember(newMember);
+  });
+  
+  client.login(process.env.DISCORD_KEY);
+}
+
+main();
